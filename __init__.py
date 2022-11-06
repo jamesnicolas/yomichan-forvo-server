a9a6f86553779a36927ad61010b34f582b04c738
James Nicolas <jamesnicolas@me.com>
2022-11-06 14:41:52 -0500

    remove requests_toolkit dependency

--- __init__.py
+++ __init__.py
@@ -5,7 +5,6 @@ import re
 import json
 import base64
 import threading
-import cloudscraper
 import logging
 
 from http import HTTPStatus
@@ -17,6 +16,13 @@ from typing import List
 
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+
+# Import 3rd party packages not included with Anki
+import sys
+import os
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent / "vendor"))
+import cloudscraper
 
 
 # Config default values
@@ -41,30 +47,30 @@ class Forvo():
     def __init__(self, config=_forvo_config):
         self.config = config
         self._set_session()
-        self.scraper = cloudscraper.create_scraper()
 
     def _set_session(self):
-        """
-        Sets the session with basic backoff retries.
-        Put in a separate function so we can try resetting the session if something goes wrong
-        """
-        retry_strategy = Retry(
-            total=3,
-            backoff_factor=1,
-            status_forcelist=[429, 500, 502, 503, 504],
-            method_whitelist=["HEAD", "GET", "OPTIONS"]
-        )
-        adapter = HTTPAdapter(max_retries=retry_strategy)
-        self.session = requests.Session()
-        self.session.mount("https://", adapter)
-        self.session.mount("http://", adapter)
-        # Use my personal user agent to try to avoid scraping detection
-        self.session.headers.update(
-            {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.27",
-                "Accept-Language": "en-US,en;q=0.5",
-            }
-        )
+        # """
+        # Sets the session with basic backoff retries.
+        # Put in a separate function so we can try resetting the session if something goes wrong
+        # """
+        # retry_strategy = Retry(
+        #     total=3,
+        #     backoff_factor=1,
+        #     status_forcelist=[429, 500, 502, 503, 504],
+        #     method_whitelist=["HEAD", "GET", "OPTIONS"]
+        # )
+        # adapter = HTTPAdapter(max_retries=retry_strategy)
+        # self.session = requests.Session()
+        # self.session.mount("https://", adapter)
+        # self.session.mount("http://", adapter)
+        # # Use my personal user agent to try to avoid scraping detection
+        # self.session.headers.update(
+        #     {
+        #         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.27",
+        #         "Accept-Language": "en-US,en;q=0.5",
+        #     }
+        # )
+        self.session = cloudscraper.create_scraper()
 
     def _get(self, path):
         """
@@ -72,7 +78,7 @@ class Forvo():
         """
         url = self._SERVER_HOST + path
         #try:
-        return self.scraper.get(url).text
+        return self.session.get(url).text
 
         # except Exception:
         #     self._set_session()
